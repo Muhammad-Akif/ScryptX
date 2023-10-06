925d2b5aec9c52572359a6b61e22ed306a45f1f6
Muhammad-Akif <muhammadakif2917@gmail.com>
2023-10-06 13:02:16 +0500

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useEffect } from "react";
 import BookDemoBanner from "@/components/LandingPage/BookDemoBanner";
 import MainView from "@/components/LandingPage/MainView";
 import PeopleUsing from "@/components/LandingPage/PeopleUsing";
@@ -6,7 +7,6 @@ import PrivacyBanner from "@/components/LandingPage/PrivacyBanner";
 import PrivacyTrust from "@/components/LandingPage/PrivacyTrust";
 import RapidBenefits from "@/components/LandingPage/RapidBenefits";
 import TrustedPeopleBanner from "@/components/LandingPage/TrustedPeopleBanner";
-import { useEffect } from "react";
 import AOS from "aos";
 import 'aos/dist/aos.css'; // Import AOS CSS
 
